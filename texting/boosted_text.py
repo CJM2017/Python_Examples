a316264c1e49a5d9f2a3839dc97755606a79c883
connor mccann <cjmccann@bu.edu>
2017-06-13 16:50:30 -0400

    texting service is running

---
+++ texting/boosted_text.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+import sys
+import json
+from twilio.rest import Client
+
+
+contacts = {'char':'16313745510'}
+okToSend = True
+
+def buildString(words):
+	message = ''
+	for word in words:
+		message += word
+		message += " "
+	return message
+
+def main():
+	# Twilio connection 
+	configFilePath = "../../twilio_account/config.json"
+	with open(configFilePath) as jsonFile:
+		account = json.load(jsonFile)
+	client = Client(account['setup']['accountSid'], account['setup']['authToken'])
+
+	# Message details
+	recipient = sys.argv[1]
+	number = contacts[recipient]
+	msgString = ''
+	words = sys.argv[2:]
+	numWords = len(words)
+
+	# Build the string
+	if (numWords >= 1):
+		msgString = buildString(words)
+		print(number)
+		print(msgString)
+
+	if (okToSend):
+		message = client.api.account.messages.create(to=number,
+												 from_="16179967175", # Somerville Area Code
+												 body=msgString)
+
+	return
+
+if __name__ == "__main__":
+	sys.exit(main())
